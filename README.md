ad0012f47ef41b77ba30279ec407c0662b9b014e
hyomin1 <leehm123456@gmail.com>
2024-04-05 10:45:58 +0900

    add jinja2 template

--- README.md
+++ README.md
@@ -5,3 +5,15 @@
 1. fastapi : 웹 프레임워크 + API
 2. uvicorn: WAS(웹 어플리케이션 서버)
 3. jinja2: 템플릿 엔진(HTML, CSS, JS)
+
+### Web 프로그래밍 기초 설명
+
+#### 1.URL
+
+    - http://127.0.0.1:8080 = http://localhost:8000
+    - 127.0.0.1과 localhost는 루프백 주소(현재 디바이스의 IP를 의미)
+    - http -> 프로토콜
+    - 8000 -> Port 번호
+    - http 프로토콜에서 제공하는 함수(get, post, put, delete)
+    - http://127.0.0.1:8000/member?id=abc1234&name=cherry -> 쿼리스트링(get 방식)
+    - 숨겨야하는 정보들(post 방식)
--- main.py
+++ main.py
@@ -2,11 +2,12 @@
 
 import uvicorn 
 from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates/")
 
-@app.get("/")
-async def welcome() -> dict:
-    return {
-        "message": "Hello FastAPI!"
-    }
+
+@app.get("/") # http://127.0.0.1:8000/
+async def welcome(request:Request) -> dict:
+    return templates.TemplateResponse("index.html",{"request": request})
