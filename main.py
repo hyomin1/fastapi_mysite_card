c217572771d9bc785c0cecdf1eefd12c7a98db4d
hyomin1 <leehm123456@gmail.com>
2024-04-09 11:42:11 +0900

    Add static file

--- main.py
+++ main.py
@@ -3,11 +3,13 @@
 import uvicorn 
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates/")
+app.mount("/static",StaticFiles(directory="static"), name="static")
 
 
 @app.get("/") # http://127.0.0.1:8000/
-async def welcome(request:Request) -> dict:
+async def welcome(request:Request):
     return templates.TemplateResponse("index.html",{"request": request})
