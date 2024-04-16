c44fd412f8b00abbe46520d6a19de946210a037c
hyomin1 <leehm123456@gmail.com>
2024-04-16 12:45:00 +0900

    use token kakaotalk me

--- README.md
+++ README.md
@@ -22,3 +22,23 @@
 
 - 인증코드 URL(Base) : https://kauth.kakao.com/oauth/authorize?client_id={REST API 키}&redirect_uri={Redirect URI}&response_type=code&scope=talk_message
 - 인증 코드 URL(Me): https://kauth.kakao.com/oauth/authorize?client_id=1872018566f45c5add75690ea13d2d09&redirect_uri=http://127.0.0.1:8000&response_type=code&scope=talk_message
+
+#### 1. 카카오 API 용어
+
+- 인증코드 : 1회성, 토큰(Access, Refresh)
+  발급받기 위해 사용!
+- Access 토큰: 카카오 API 서비스를 이용할 때 사용
+- Refresh 토큰: Access 토큰을 재발급 받기 위해 사용
+- 생명주기: 인증코드(1회), Access(6시간), Refresh(2달)
+- \*Refresh Token은 발급받고 1달 후부터 재발급 가능
+- Access와 Refresh 재발급 받는 코드는 동일
+- ㄴ 재발급 코드 : Refresh 발급받은지 1달 미만, Access 토큰만 재발급 해서 리턴
+- ㄴ 재발급 코드 : Refresh 발급받은지 1달 이상, Refresh 토큰 재발급 해서 리턴
+
+#### 2. 카카오 API 사용 방법
+
+1. Kakao Developer 사이트에서 "권한 허용 및 동의"
+2. 웹브라우저 URL을 통해서 인증 코드 발급
+3. 인증코드 사용해서 토큰 (Access, Refresh)발급
+4. Access 사용해서 서비스 이용!
+5. - 1달에 한번씩 Refresh 토큰 재발급 스케줄링
--- kakao_api.py
+++ kakao_api.py
@@ -4,7 +4,7 @@
 # 3. 인증 코드를 사용해서 토큰 발급
 # 4. 토큰을 사용해서 나에게 메세지 보내기
 import requests
-
+import json
 
 # 1. 카카오 OAuth URL과 Redirect Key를 사용해서 인증 코드 요청
 # - 웹 브라우저 URL:https://kauth.kakao.com/oauth/authorize?client_id=1872018566f45c5add75690ea13d2d09&redirect_uri=http://127.0.0.1:8000&response_type=code&scope=talk_message
@@ -17,9 +17,28 @@ data = {
     "grant_type" : "authorization_code",
     "client_id" : "1872018566f45c5add75690ea13d2d09",    # RESTAPI KEY
     "redirect_uri" :"http://127.0.0.1:8000",  
-    "code":"R-JtQQykZwz4vzAJ1ecwvrhxDoMf-8azeCfo8fqF2X1KoqOV5dBw7h_TKLcKKcjaAAABjsDrS3rHP8VuE1ZNOQ",
+    "code":"gRPQbvOAP-E-WyZRo7j8DPiw-Mz6WA_0QMC3U33IiaTmwIfgpRPE9du3_g4KKiVOAAABjuTooFEtjdRiIM79qQ",
 }
 
-response = requests.post(url, data=data)
-tokens = response.json()
-print(tokens)
+
+
+
+
+
+
+
+# 3. 토큰 이용해서 나에게 톡 보내기
+access_token="QXpYKu4YYJsrhkaKNET-P42WD8ip7T6IPVEKPXSZAAABjuTq3qfdCc_9be4aqQ"
+msg_url = "https://kapi.kakao.com/v2/api/talk/memo/default/send"
+headers = {
+    "Authorization" : "Bearer " + access_token
+}
+msg_data = {
+    "template_object": json.dumps({
+        "object_type": "text",
+        "text":"카카오톡 테스트",
+        "link":{"mobile_web_url":"https://www.naver.com"}
+    })
+}
+response = requests.post(msg_url, headers=headers, data=msg_data)
+
